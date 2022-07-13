993707c6fc7a72b7f9ebd5df92487075d99162f1
fantasyfanwuren <756423901@qq.com>
2022-07-13 11:55:20 +0800

    cmdCommand class complete

--- cmdcommand.cpp
+++ cmdcommand.cpp
@@ -1,5 +1,11 @@
 #include "cmdcommand.h"
 #include <QProcess>
+#include <QEventLoop>
+#include <QTimer>
+#include <QUrl>
+#include <QCryptographicHash>
+
+
 
 CMDCommand::CMDCommand(QObject *parent)
     : QObject{parent}
@@ -7,6 +13,7 @@ CMDCommand::CMDCommand(QObject *parent)
     cmd = new QProcess(this);
     arg<<"/c"<<"chia wallet nft mint";
 }
+
 
 bool CMDCommand::setWorkDir(const QString & dir)
 {
@@ -20,10 +27,13 @@ bool CMDCommand::setWorkDir(const QString & dir)
 
 bool CMDCommand::setFingerprint(const QString &f)
 {
+    /*
+     * 设置指纹
+     */
     if(f.isEmpty())
         return false;
     fingerprint = f;
-    arg<<"-f"<<fingerprint;
+    universalArg<<"-f"<<fingerprint;
     return true;
 
 }
@@ -31,16 +41,22 @@ bool CMDCommand::setFingerprint(const QString &f)
 
 bool CMDCommand::setNFTID(const QString & i)
 {
+    /*
+     * 设置NFT钱包ID
+     */
     if(i.isEmpty())
         return false;
     NFTID = i;
-    arg<<"-i"<<NFTID;
+    universalArg<<"-i"<<NFTID;
     return true;
 }
 
 
 bool CMDCommand::setUris(const QStringList &u)
 {
+    /*
+     * 设置图片链接集合
+     */
     if(u.isEmpty())
         return false;
     uris = u;
@@ -50,6 +66,9 @@ bool CMDCommand::setUris(const QStringList &u)
 
 bool CMDCommand::setmetadataUris(const QStringList &mu)
 {
+    /*
+     * 设置元数据链接集合
+     */
     if(mu.isEmpty())
         return false;
     metadataUris = mu;
@@ -59,60 +78,211 @@ bool CMDCommand::setmetadataUris(const QStringList &mu)
 
 bool CMDCommand::setLicenseUris(const QString &lu)
 {
+    /*
+     * 设置许可证
+     */
     if(lu.isEmpty())
         return false;
     licenseUris = lu;
-    arg<<"-lu"<<licenseUris;
+    universalArg<<"-lu"<<licenseUris;
     return true;
 }
 
 
 bool CMDCommand::setRoyaltyAddress(const QString &ra)
 {
+    /*
+     * 设置版税接受地址
+     */
     if(ra.isEmpty())
         return false;
     royaltyAddress = ra;
-    arg<<"-ra"<<royaltyAddress;
+    universalArg<<"-ra"<<royaltyAddress;
     return true;
 }
 
 
 bool CMDCommand::setRoyaltyPercentage(const QString &rp)
 {
+    /*
+     * 设置版税比例
+     */
     if(rp.isEmpty())
         return false;
     royaltyPercentage = rp;
-    arg<<"-rp"<<royaltyPercentage;
+    universalArg<<"-rp"<<royaltyPercentage;
     return true;
 }
 
 
 bool CMDCommand::setTargetAddress(const QString &ta)
 {
+    /*
+     * 设置目标地址
+     */
     if(ta.isEmpty())
         return false;
     targetAddress = ta;
-    arg<<"-ta"<<targetAddress;
+    universalArg<<"-ta"<<targetAddress;
     return true;
 }
 
 
 bool CMDCommand::setSeriesNumber(const QString &sn)
 {
+    /*
+     * 设置版本号
+     */
     if(sn.isEmpty())
         return false;
     seriesNumber = sn;
-    arg<<"-sn"<<seriesNumber;
+    universalArg<<"-sn"<<seriesNumber;
     return true;
 }
 
 
 bool CMDCommand::setSeriesTotal(const QString &st)
 {
+    /*
+     * 设置总版本号
+     */
     if(st.isEmpty())
         return false;
     seriesTotal = st;
-    arg<<"-st"<<seriesTotal;
+    universalArg<<"-st"<<seriesTotal;
     return true;
 }
+
+void CMDCommand::on_downloadProgress(qint64 bytesReceived, qint64 bytesTotal)
+{
+    /*
+     * 通过信号downLoadPercentage，向GUI发送下载过程的各项信息，方便在GUI显示
+     */
+    emit downLoadPercentage(bytesReceived,bytesTotal,currentRow,currentColumn);
+}
+
+
+QString CMDCommand::getSha256(const QByteArray &array) const
+{
+    /*
+     * 一个计算哈希的小函数
+     */
+    QCryptographicHash hash(QCryptographicHash::Sha256);
+        hash.addData(array);  // 添加数据到加密哈希值
+        QByteArray result = hash.result();  // 返回最终的哈希值
+        QString strSha256 = result.toHex();
+        return strSha256;
+}
+
+void CMDCommand::on_finished()
+{
+    /*
+     * 下载完成后将根据currentColumn的值确定是下载的图片、元数据或者许可证
+     * 计算哈希，并对arg进行更新
+     * 发出哈希计算完成信号，将currentRow,currentColumn和哈希值发送出去，方便界面显示
+     * 清空下载请求指针
+     */
+    emit downLoadfinished(currentRow,currentColumn);
+    downLoadState = true;
+    QByteArray array = reply->readAll();
+    QString sha256 = getSha256(array);
+    emit hashfinishde(currentRow,currentColumn,sha256);
+    //0代表文件，1代表图片哈希，2代表元数据哈希，3代表许可证哈希
+    if(currentColumn==1){
+        arg<<"-nh"<<sha256;
+    }else if(currentColumn == 2){
+        arg<<"-mh"<<sha256;
+    }else if(currentColumn == 3){
+        arg<<"-lh"<<sha256;
+    }
+    reply->deleteLater();
+}
+
+void CMDCommand::wait(int sec)
+{
+    /*
+     * 一个延迟小函数，方便输出下载超时信息
+     */
+    for(int i=0;i<sec;++i){
+            QEventLoop eventloop;
+            QTimer::singleShot(1000, &eventloop, SLOT(quit()));
+            eventloop.exec();
+        }
+}
+
+
+bool CMDCommand::downLoad(const QString & urlSpace)
+{
+    /* 根据具体的链接文本，对链接进行下载，
+     * 若下载成功，链接槽函数on_finished()
+     * 下载过程链接槽函数on_downloadProgress(qint64, qint64);
+     * 若失败将发送downFail(currentRow,currentColumn)
+     */
+    if(urlSpace.isEmpty()){
+        emit downFail(currentRow,currentColumn);
+        return false;
+    }
+    QUrl newUrl = QUrl(urlSpace);
+    if(!newUrl.isValid()){
+        emit downFail(currentRow,currentColumn);
+        return false;
+    }
+    downLoadState = false;
+    reply = networkManager.get(QNetworkRequest(newUrl));
+    connect(reply,SIGNAL(finished()),this,SLOT(on_finished()));
+    connect(reply,SIGNAL(downloadProgress(qint64, qint64))
+                ,this,SLOT(on_downloadProgress(qint64, qint64)));
+    int timeout = 30;
+    for(int i=0;i<timeout;++i){
+            wait(1);
+            if(downLoadState == false){
+                break;
+            }
+            if(i==timeout-1){
+                emit downFail(currentRow,currentColumn);
+                return false;
+            }
+    }
+    return true;
+
+}
+
+
+int CMDCommand::run()
+{
+    /* 在进行run之前，必须提前进行所有的set功能，完成对universalArg的初始化
+     * 然后进行 row循环，内嵌column循环，
+     * 在row循环中，需要对arg清空并赋值 universalArg
+     * column循环中从1到3；分别为：1代表图片哈希，2代表元数据哈希，3代表许可证哈希
+     *
+     * 要点1：图片，元数据的列表级其他universalArg在外部创建
+     * 要点2：要时刻控制 currentRow,currentColumn两个变量，方便信号的输出和对全局的把控
+     * 返回值：0成功，1图片无法下载，2元数据无法下载，3许可证无法下载
+     */
+
+    for(currentRow = 0;currentRow<uris.count();++currentRow){
+        arg.clear();
+        arg = universalArg;
+        qDebug()<<"初始化的arg:"<<arg;
+        currentColumn = 1;//图片
+        arg<<"-u"<<uris.at(currentRow);
+        if(!downLoad(uris.at(currentRow)))
+            return 1;
+
+        currentColumn = 2; //元数据
+        arg<<"-mu"<<metadataUris.at(currentRow);
+        if(!downLoad(metadataUris.at(currentRow)))
+            return 2;
+
+        currentColumn = 3; //许可证
+        if(!downLoad(licenseUris))
+            return 3;
+        //执行cmd命名
+        qDebug()<<"需要执行的arg："<<arg;
+    }
+    return 0;
+
+
+}
+
 
--- cmdcommand.h
+++ cmdcommand.h
@@ -2,6 +2,7 @@
 #define CMDCOMMAND_H
 
 #include <QObject>
+#include <QtNetwork>
 
 class QProcess;
 
@@ -11,23 +12,41 @@ class CMDCommand : public QObject
 public:
     explicit CMDCommand(QObject *parent = nullptr);
 
-private:
+public:
     bool setWorkDir(const QString & dir);
     bool setFingerprint(const QString &f);
     bool setNFTID(const QString & i);
-    bool setUris(const QStringList &u);
-    bool setmetadataUris(const QStringList &mu);
-    bool setLicenseUris(const QString &lu);
+    bool setUris(const QStringList &u); //利用循环处理
+    bool setmetadataUris(const QStringList &mu);//利用循环处理
+    bool setLicenseUris(const QString &lu); //简化代码量，减少出错率，同样在循环中处理
     bool setRoyaltyAddress(const QString &ra);
     bool setRoyaltyPercentage(const QString &rp);
     bool setTargetAddress(const QString &ta);
     bool setSeriesNumber(const QString &sn);
     bool setSeriesTotal(const QString &st);
+    int run();
+
+private:
+    void wait(int sec);
+    bool downLoad(const QString & urlSpace);
+    QString getSha256(const QByteArray &array) const;
 
 signals:
+    void downLoadPercentage(qint64,qint64,int,int);
+    void downLoadfinished(int,int);
+    void downFail(int,int);
+    void hashfinishde(int,int,QString);
+
+private slots:
+    void on_downloadProgress(qint64 bytesReceived, qint64 bytesTotal);
+    void on_finished();
+
 private:
+    QNetworkReply * reply;
+    QNetworkAccessManager networkManager;
     QProcess *cmd;
     QStringList arg;//CMD参数
+    QStringList universalArg;//CMD全局参数备份
     QString workDir; //工作环境
     QString fingerprint; //指纹
     QString NFTID; //NFT钱包ID
@@ -39,6 +58,9 @@ private:
     QString targetAddress; //目标地址
     QString seriesNumber; //版本号
     QString seriesTotal; // 总版本号
+    bool downLoadState = false;
+    int currentRow; //代表列表中的文件
+    int currentColumn;//0代表文件，1代表图片哈希，2代表元数据哈希，3代表许可证哈希
 };
 
 #endif // CMDCOMMAND_H
